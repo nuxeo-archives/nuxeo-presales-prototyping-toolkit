5a59ba2a5412a25ca746551513fc4ddc44b4140b
Karl Harris <kharris@nuxeo.com>
2013-06-25 09:22:05 -0400

    PRESALE-24 Fixed client vs core session, added dependencies for commons-net missing when run on Nuxeo server.

--- nuxeo-presales-prototyping-toolkit-operations/src/main/java/org/nuxeo/presales/prototyping/toolkit/operations/ftp/BlobExport.java
+++ nuxeo-presales-prototyping-toolkit-operations/src/main/java/org/nuxeo/presales/prototyping/toolkit/operations/ftp/BlobExport.java
@@ -17,28 +17,28 @@ import org.apache.commons.vfs2.FileSystemManager;
 import org.apache.commons.vfs2.FileSystemOptions;
 import org.apache.commons.vfs2.VFS;
 import org.apache.commons.vfs2.provider.ftp.FtpFileSystemConfigBuilder;
-import org.nuxeo.ecm.automation.client.Session;
 import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.automation.core.annotations.Param;
 import org.nuxeo.ecm.automation.core.collectors.DocumentModelCollector;
+import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
 
 /**
- * @author stephane lancone, karlharris
+ * @author stephane lancoine, karlharris
  * 
- * This Operation takes a single Parameter. A URI of the form <protocol>://<username>:<password>@hostname/<folder(s)>/filename.
+ * This Operation takes a single Parameter. A URI of the form <protocol>://<username>:<password>@hostname/<folder>/filename.
  * Takes as input a single Document or a List of Documents each document must be of type File and contain a blob of bytes. The Operation
  * will uses the Apache VFS software to transfer the file using the <protocol> named in the URI. 
  * 
  */
 
 
-@Operation(id = BlobExport.ID, category = Constants.CAT_BLOB, label = "BlobExport", description = "Export via Apache VFS")
+@Operation(id = BlobExport.ID, category = Constants.CAT_DOCUMENT, label = "BlobExport", description = "Export Blob from a Document use URI format:<protocol>://<username>:<password>@hostname/<folder>/filename " )
 public class BlobExport {
 
 	public static final String ID = "BlobExport";
@@ -50,7 +50,7 @@ public class BlobExport {
 	protected Log log = LogFactory.getLog(BlobExport.class);
 
 	@Context
-	Session session;
+	CoreSession coresession;
 
 	@Param(name = "URI")
 	String uriParams;
